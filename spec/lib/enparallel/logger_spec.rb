e96c7d91ede2addb6475eaa0f97ea9da0bed91c1
crdx <crdx@users.noreply.github.com>
2019-08-19 12:34:04 +0100

    Clean up logger tests

--- spec/lib/enparallel/logger_spec.rb
+++ spec/lib/enparallel/logger_spec.rb
@@ -1,29 +1,23 @@
 describe Logger do
-    def get_logger(type)
+    def get_logs(type)
         pool = get_pool_of(5, type)
         pool.drain_wait
-        pool.logger
-    end
-
-    def get_logs(type)
-        get_logger(type).get_logs
+        pool.get_logs
     end
 
     it 'logs successful tasks' do
         logs = get_logs(:good_command)
-        log = logs.first
 
         expect(logs.length).to eq(1)
-        expect(log.type).to eq(:success)
-        expect(log.content).to include('echo potato')
+        expect(logs.first.type).to eq(:success)
+        expect(logs.first.content).to include('echo potato')
     end
 
     it 'logs failed tasks' do
         logs = get_logs(:bad_command)
-        log = logs.first
 
         expect(logs.length).to eq(1)
-        expect(log.type).to eq(:failure)
-        expect(log.content).to include('ekko potato')
+        expect(logs.first.type).to eq(:failure)
+        expect(logs.first.content).to include('ekko potato')
     end
 end

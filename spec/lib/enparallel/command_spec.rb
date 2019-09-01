04dfcdf8cbd2b539cb0fda0f2623406508477e63
crdx <crdx@users.noreply.github.com>
2019-09-01 23:16:07 +0100

    Fix & add test

--- spec/lib/enparallel/command_spec.rb
+++ spec/lib/enparallel/command_spec.rb
@@ -1,6 +1,11 @@
 describe Command do
-    it 'replaces arguments correctly' do
+    it 'replaces arguments correctly (safely)' do
         command = Command.from_a(['ls', '{}'])
-        expect(command.interpolate('cheese')).to eq('ls cheese')
+        expect(command.interpolate_safe('potato pie')).to eq('ls potato\\ pie')
+    end
+
+    it 'replaces arguments correctly (unsafely)' do
+        command = Command.from_a(['ls', '{}'])
+        expect(command.interpolate_unsafe('potato pie')).to eq('ls potato pie')
     end
 end
